8d9119db71c8a4389437317869689d0f0c808eb7
Nikita Vlaskin <sinezeleuny@gmail.com>
2026-02-28 23:31:39 +0100

    Stubs

---
+++ 01_variables_expressions_assignments/1_problems_with_no_arrays/1-1-19.lua
@@ -0,0 +1,11 @@
+-- Problem 1.1.19
+
+function euclide(m, n)
+    
+end
+
+-- Test
+print(euclide(4, 8))
+print(euclide(32, 16))
+print(euclide(0, 9))
+print(euclide(78, 45))
---
+++ 01_variables_expressions_assignments/2_arrays/1-2-7.lua
@@ -0,0 +1,9 @@
+-- Problem 1.2.7
+
+function count(arr)
+    
+end
+
+-- Test
+local arr = {5, 4, 3, 3, 2, 5, 6, 9, 6, 6, 7, 9, 0}
+print(count(arr))
