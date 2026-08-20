eee53ed93a0c2dd12e4381207e527e892bcf2d81
Nikita <80875524+enarve@users.noreply.github.com>
2026-08-20 13:03:14 +0200

    Problem 5.1.3

---
+++ 05_text_processing/1_compound_symbols/5-1-3.lua
@@ -0,0 +1,27 @@
+-- Problem 5.1.3
+
+local input_file = io.open("input.txt", "r")
+local output_file = io.open("output.txt", "w")
+
+assert(input_file, "No input file found.")
+assert(output_file, "No output file found.")
+
+local comment_started = false
+
+local char = input_file:read(1)
+while char do
+    if comment_started then
+	    if char == "}" then
+            comment_started = false
+			output_file:write(" ")
+        end
+    else
+        if char == "{" then
+            comment_started = true
+        else
+            output_file:write(char)
+        end
+    end
+    -- move to next char
+    char = input_file:read(1)
+end
